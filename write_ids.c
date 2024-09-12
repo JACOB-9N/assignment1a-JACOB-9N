5d873f1fcddbdfd6b4768f434c151ed9f09ad909
JACOB-9N <jmn6010@psu.edu>
2024-09-12 13:05:29 -0400

    Update write_ids.c

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "JACOB-9N";
+    char STUDENT_ID[] = "jmn6010@psu.edu";
 
     return 0 ;
 }
